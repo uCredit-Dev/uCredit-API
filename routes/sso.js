0e5346d78452537d31782f8601790db28c750803
Olivia Wu <qinyuan.wupsn@gmail.com>
2021-02-24 17:26:52 +0800

    pk created, metadata is working

--- routes/sso.js
+++ routes/sso.js
@@ -2,16 +2,25 @@
 //change secret
 //process user attribute
 //contact enterpriseauth@jhmi.edu to be added to the trusted SP list
+require("dotenv").config();
 const express = require("express");
 const passport = require("passport");
 const saml = require("passport-saml");
 const session = require("express-session");
 const bodyParser = require("body-parser");
 const fs = require("fs");
+const path = require("path");
 
 const router = express.Router();
-const PbK = fs.readFileSync(__dirname + "/certs/cert.pem", "utf8");
-const PvK = fs.readFileSync(__dirname + "/certs/key.pem", "utf8");
+const PbK = fs.readFileSync(
+  path.resolve(__dirname, "../certs/cert.pem"),
+  "utf8"
+);
+const PvK = fs.readFileSync(
+  path.resolve(__dirname, "../certs/key.pem"),
+  "utf8"
+);
+const secret = process.env.SESSION_SECRET;
 
 const JHU_SSO_URL = "https://idp.jh.edu/idp/profile/SAML2/Redirect/SSO";
 const SP_NAME = "ucredit";
@@ -46,9 +55,7 @@ passport.deserializeUser(function (user, done) {
 
 // Middleware
 router.use(bodyParser.urlencoded({ extended: false }));
-router.use(
-  session({ secret: "secret", resave: false, saveUninitialized: true })
-);
+router.use(session({ secret: secret, resave: false, saveUninitialized: true }));
 router.use(passport.initialize({}));
 router.use(passport.session({}));
 
@@ -70,7 +77,9 @@ router.post(
   passport.authenticate("samlStrategy"),
   (req, res) => {
     // the user data is in req.user
-    res.send(`welcome ${req.user.first_name}`);
+    res.send(
+      `welcome ${req.user.first_name} ${req.user.last_name}, JHED id: ${req.user.id}, affiliation: ${req.user.affiliation}, job title: ${req.user.user_field_job_title}, email: ${req.user.email}`
+    );
   }
 );
 
@@ -80,3 +89,5 @@ router.get("/api/metadata", (req, res) => {
   res.status(200);
   res.send(samlStrategy.generateServiceProviderMetadata(PbK, PbK));
 });
+
+module.exports = router;
