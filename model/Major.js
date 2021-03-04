ec7a91307cd3e0ee6bdc19bbd3e0b1aee13011b6
Olivia Wu <qinyuan.wupsn@gmail.com>
2021-03-04 13:30:58 +0800

    name & department only

---
+++ model/Major.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+/*  
+    This model refers to a student's academic major, and stores
+    distribution requirements.
+*/
+const majorSchema = new Schema({
+    name: { type: String, required: true },
+    department: { type: String, required: true },
+    
+});
