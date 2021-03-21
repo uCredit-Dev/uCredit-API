3403f95c27197513d43519bce5d520ded4a4a0a8
Qinyuan Wu <qinyuan.wupsn@gmail.com>
2021-03-22 00:21:26 +0800

    search by name or title

--- routes/search.js
+++ routes/search.js
@@ -1,1 +1,19 @@
 //routes to handle search requests
+const express = require("express");
+const router = express.Router();
+const { returnData, errorHandler } = require("./helperMethods.js");
+const SISCourses = require("../model/SISCourse.js");
+
+router.get("/api/search/:query", (req, res) => {
+  const query = req.params.query;
+  SISCourses.find({
+    $or: [
+      { title: { $regex: query, $options: "i" } },
+      { number: { $regex: query, $options: "i" } },
+    ],
+  })
+    .then((match) => returnData(match, res))
+    .catch((err) => errorHandler(res, 500, err));
+});
+
+module.exports = router;
