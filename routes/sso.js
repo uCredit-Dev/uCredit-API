640fa1a011b00986e38a0ef698b1daba1ec530c7
MattLiu-mygit <mliu78@jh.edu>
2021-05-07 23:05:12 -0400

    Update sso.js

--- routes/sso.js
+++ routes/sso.js
@@ -60,7 +60,7 @@ router.use(
     secret: secret,
     resave: false,
     saveUninitialized: true,
-    // cookie: { sameSite: "none", secure: true },
+    cookie: { sameSite: "none", secure: true },
   })
 );
 router.use(passport.initialize({}));
