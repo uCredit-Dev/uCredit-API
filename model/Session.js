486f8b983620841f7122804e16f9ffbcb7cc1ece
MattLiu-mygit <mliu78@jh.edu>
2021-05-26 23:33:41 -0400

    Update Session.js

--- model/Session.js
+++ model/Session.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 
 const sessionSchema = new Schema({
   createdAt: { type: Date, expires: 60 * 60 * 24 * 1000, default: Date.now() }, //60*60*24
-  expiresAt: { type: Date, default: Date.now() }, //60*60*24
+  expireAt: { type: Date, default: Date.now() }, //60*60*24
   // sessionActivity: { type: Date, expires: "15m", default: Date.now() },
   _id: { type: String },
   hash: { type: String },
