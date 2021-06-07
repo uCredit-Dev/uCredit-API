e9268ccafa515c100d36df45ff4e40c61694d0e6
Qinyuan Wu <qinyuan.wupsn@gmail.com>
2021-06-08 02:33:12 +0800

    new SIS course schema

---
+++ model/SISCourseV.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+/*  
+    This model refers to courses retrieved from the SIS API.
+*/
+
+const courseSchema = new Schema({
+  title: { type: String, required: true },
+  number: { type: String, required: true },
+  terms: [{ type: String, required: true }],
+  versions: [
+    {
+      areas: { type: String, required: true },
+      term: { type: String, required: true },
+      school: { type: String, required: true },
+      department: { type: String, default: "Unspecified" },
+      credits: { type: Number, required: true },
+      wi: { type: Boolean, required: true },
+      bio: { type: String },
+      level: { type: String },
+      tags: [{ type: String }],
+      preReq: { type: Array },
+      coReq: { type: Array },
+      restrictions: { type: Array },
+    },
+  ],
+});
+
+const SISCourseV = mongoose.model("SISCourseV", courseSchema);
+
+module.exports = SISCourseV;
