e0097d9130e65d561fb6f183af47a8eab3e1bb45
MattLiu-mygit <mliu78@jh.edu>
2021-06-16 15:59:40 -0400

    Update search.js

--- routes/search.js
+++ routes/search.js
@@ -14,13 +14,16 @@ router.get("/api/search/all", (req, res) => {
 router.get("/api/search/skip/:num", (req, res) => {
   const toSkip = Math.parseInt(req.params.num);
   SISCV.find({})
-    .skip(toSkip)
-    .limit(500)
     .then((courses) => {
-      const out = courses.slice(0, toSkip);
+      const out = courses.slice(toSkip, toSkip * 2);
       return returnData(out, res);
     })
-    .catch((err) => errorHandler(res, 500, err));
+    .catch((err) =>
+      errorHandler(res, 500, {
+        ...err,
+        message: "toSkip is " + toSkip + " out length is " + out.length,
+      })
+    );
 });
 
 //return the term version of a specific course
