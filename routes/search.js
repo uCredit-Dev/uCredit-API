f0131a1f220084cd6c7069956ceab752b963275a
MattLiu-mygit <mliu78@jh.edu>
2021-06-16 16:37:04 -0400

    Update search.js

--- routes/search.js
+++ routes/search.js
@@ -14,7 +14,7 @@ router.get("/api/search/all", (req, res) => {
 router.get("/api/search/skip/:num", (req, res) => {
   const toSkip = req.params.num;
   SISCV.find({})
-    .limit(500)
+    .limit(750)
     .then((courses) => returnData(courses, res))
     .catch((err) => errorHandler(res, 500, err));
 });
