df8e2e9408cfcccdcde5f2a4d0708f2da9362008
will tong <46694175+willtong77@users.noreply.github.com>
2021-12-08 14:59:29 -0500

    changed variable name

--- routes/evaluation.js
+++ routes/evaluation.js
@@ -1,12 +1,12 @@
 const { returnData, errorHandler } = require("./helperMethods.js");
-const eval = require("../model/Evaluation.js");
+const evaluation = require("../model/Evaluation.js");
 const express = require("express");
 const router = express.Router();
 
 //get course evaluations by course number
 router.get("/api/evals/:number", (req, res) => {
   const num = req.params.number;
-  eval
+  evaluation
     .findOne({ num })
     .then((review) => returnData(review, res))
     .catch((err) => errorHandler(res, 500, err));
