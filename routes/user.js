d8ce86c7c3ddbadc6a663f8624e3ca6aaf987fe8
mtiavis1 <mtiavis1@jhu.edu>
2021-12-29 19:29:05 -0500

    feat: Added route to retrieve all users

--- routes/user.js
+++ routes/user.js
@@ -1,2 +1,17 @@
 //routes to handle user info modification & validation
 //update grade?
+
+const users = require("../model/User.js");
+const { returnData, errorHandler } = require("./helperMethods.js");
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/api/users/all", (req, res) => {
+  users
+    .find({})
+    .then((user) => returnData(user, res))
+    .catch((err) => errorHandler(res, 500, err));
+});
+
+module.exports = router;
