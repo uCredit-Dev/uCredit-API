9fc4f9326c60d0b3496de0478e6f3db38a7203fa
James S. Wang <JJamesWWang@gmail.com>
2022-01-11 20:39:28 -0500

    Plans: Add unit test for creating plan

---
+++ routes/plan.spec.ts
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const { returnData } = require("./helperMethods");
+const plans = require("../model/Plan");
+const createApp = require("../app");
+
+beforeEach((done) => {
+  mongoose
+    .connect("mongodb://localhost:27017/majors", { useNewUrlParser: true })
+    .then(() => done());
+});
+
+afterEach((done) => {
+  mongoose.connection.db.dropDatabase(() => {
+    mongoose.connection.close(() => done());
+  });
+});
+
+const request = supertest(createApp());
+
+describe("plan routes", () => {
+  it("should create a new plan", async () => {
+    await request
+      .post("/api/plans")
+      .send(planData)
+      .then(async () => {
+        const plan = await plans.findOne({ name: "Unnamed Plan" });
+        expect(plan).toBeTruthy();
+      });
+  });
+});
+
+const planData = {
+  name: "Unnamed Plan",
+  user_id: "guestUser",
+  majors: ["B.S. Computer Science (OLD - Pre-2021)"],
+  year: "AE UG Freshman",
+  expireAt: Date.now() + 60 * 60 * 24,
+};
+
+export { }
