c17d5747a3100c05cb134211257772c1db726bd6
Qinyuan Wu <qinyuan.wupsn@gmail.com>
2022-02-20 22:48:39 -0500

    add affiliation filter

--- routes/user.js
+++ routes/user.js
@@ -5,12 +5,14 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/api/user", (req, res) => {
-  const username = req.query.username;
+  const username = req.query.username || "";
+  const affiliation = req.query.affiliation || "";
   const query = {
     $or: [
       { _id: { $regex: username, $options: "i" } },
       { name: { $regex: username, $options: "i" } },
     ],
+    affiliation: { $regex: affiliation, $options: "i" },
   };
   users
     .find(query)
