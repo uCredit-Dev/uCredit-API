a8830c3e09f5291f41a099f2af1c0b6707bac626
James S. Wang <JJamesWWang@gmail.com>
2022-03-02 17:01:47 -0500

    Reviewer tests: Add mongo db and expect 400 (will have to change test name)

--- tests/routes/reviewer.test.js
+++ tests/routes/reviewer.test.js
@@ -1,13 +1,17 @@
 // Run test suite with: npm run test tests/routes/reviewer.test.js
 
+const mongoose = require("mongoose");
 const supertest = require("supertest");
 const createApp = require("../../app");
 
 const request = supertest(createApp());
 const endpoint = "/api/planReview/addReviewer";
 
-describe(`Test endpoint ${endpoint}`, () => {
+beforeEach(async () => {
+  await mongoose.connect(global.__MONGO_URI__);
+});
 
+describe(`Test endpoint ${endpoint}`, () => {
   describe("HTTP POST request", () => {
     // test("Return 400 if missing plan_id or reviewer_id in request body", async () => {
     //     const response1 = await request.post(`${endpoint}`).send({
@@ -21,19 +25,22 @@ describe(`Test endpoint ${endpoint}`, () => {
     // });
 
     test("Return 200 on success", async () => {
-        const response = await request.post(`${endpoint}`).send({
-            plan: "asdfjksla",
-            reviewer: "asdfjkl"
-        });
-        expect(response.status).toBe(200);
+      const response = await request.post(`${endpoint}`).send({
+        plan_id: "asdfjksla",
+        reviewer_id: "asdfjkl",
+      });
+      expect(response.status).toBe(400);
     });
     // test("Return 400 if reviewer already added on plan", async () => {
-      
+
     // });
 
     // test("Return 200 if successfully added reviewer", async() => {
 
     // });
   });
+});
 
+afterEach(async () => {
+  await mongoose.disconnect();
 });
