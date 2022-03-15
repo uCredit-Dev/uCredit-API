bab6477e1409afdf6e05dcb1e56fc733cde37058
Qinyuan Wu <qinyuan.wupsn@gmail.com>
2022-03-14 22:55:33 -0400

    restructure plan review system with new schema

--- routes/planReview.js
+++ routes/planReview.js
@@ -1,117 +1,93 @@
 const { returnData, errorHandler } = require("./helperMethods.js");
-const users = require("../model/User.js");
-const plans = require("../model/Plan.js");
+const planReviews = require("../model/PlanReview.js");
 
 const express = require("express");
 const router = express.Router();
 
-router.post("/api/planReview/request", (req, res) => {
+router.post("/api/planReview/request", async (req, res) => {
   const plan_id = req.body.plan_id;
+  const reviewee_id = req.body.reviewee_id;
   const reviewer_id = req.body.reviewer_id;
-  if (!plan_id || !reviewer_id) {
+  if (!plan_id || !reviewer_id || !reviewee_id) {
     errorHandler(res, 400, {
-      message: "Missing plan_id or reviewer_id in the request body.",
+      message:
+        "Missing plan_id, reviewee_id or reviewer_id in the request body.",
     });
+  } else {
+    const review = await planReviews
+      .findOne({ reviewer_id, reviewee_id, plan_id })
+      .exec();
+    if (review) {
+      errorHandler(res, 409, {
+        message: "Review request already created.",
+      });
+    } else {
+      const planReview = {
+        reviewee_id,
+        reviewer_id,
+        plan_id,
+        requestTime: Date.now(),
+        status: "PENDING",
+      };
+      planReviews
+        .create(planReview)
+        .then((review) => returnData(review, res))
+        .catch((err) => errorHandler(res, 500, err));
+    }
   }
-  plans
-    .findById(plan_id)
-    .then(async (plan) => {
-      if (plan.reviewers.indexOf(reviewer_id) < 0) {
-        //pull up reviewer info
-        await users
-          .findById(reviewer_id)
-          .then((user) => {
-            if (!user) {
-              errorHandler(res, 404, {
-                message: "Reviewer is not registered.",
-              });
-            }
-            //send email
-          })
-          .catch((err) => errorHandler(res, 500, err));
-        const request = {
-          user_id: reviewer_id,
-          pending: true,
-          sentTime: Date.now(),
-        };
-        plan.reviewers.push(request);
-        plan.save();
-        returnData(plan.reviewers, res);
-      } else {
-        errorHandler(res, 409, {
-          message: "Reviewer already added for this plan.",
-        });
-      }
-    })
-    .catch((err) => errorHandler(res, 400, err));
 });
 
 //reviewer confirms the request, adding the plan id to the whitelisted_plan_ids array, changing the pending status
 router.post("/api/planReview/confirm", (req, res) => {
-  const plan_id = req.body.plan_id;
-  const reviewer_id = req.body.reviewer_id;
-  if (!plan_id || !reviewer_id) {
+  const review_id = req.body.review_id;
+  if (!review_id) {
     errorHandler(res, 400, {
-      message: "Missing plan_id or reviewer_id in the request body.",
+      message: "Missing review_id in request body.",
     });
   }
-  users
-    .findById(reviewer_id)
-    .then((reviewer) => {
-      if (reviewer.whitelisted_plan_ids.indexOf(plan_id) >= 0) {
-        errorHandler(res, 409, { message: "Reviewer already confirmed." });
-      } else {
-        reviewer.whitelisted_plan_ids.push(plan_id);
-        reviewer.save();
-      }
-    })
-    .catch((err) => errorHandler(res, 500, err));
-  //change the pending status of the request
-  plans
-    .findById(plan_id)
-    .then((plan) => {
-      plan.reviewers = plan.reviewers.map((r) => {
-        if (r.user_id === reviewer_id) {
-          r.pending = false;
-        }
-        return r;
-      });
-      plan.save();
-      returnData("Reviewer confirmed.", res);
-    })
+  planReviews.findById(review_id).then((review) => {
+    if (!review) {
+      errorHandler(res, 404, { message: "planReview not found." });
+    } else if (review.status === "ACCEPTED") {
+      errorHandler(res, 400, { message: "Reviewer already confirmed." });
+    } else {
+      review.status = "ACCEPTED";
+      review.save();
+      returnData(review, res);
+    }
+  });
+});
+
+/*
+  Return a list of reviewrs for the plan with populated reviewer info
+*/
+router.get("/api/planReview/getReviewers", (req, res) => {
+  const plan_id = req.query.plan_id;
+  planReviews
+    .find({ plan_id })
+    .populate("reviewer_id", "name email affiliation school grade")
+    .then((reviews) => returnData(reviews, res))
     .catch((err) => errorHandler(res, 500, err));
 });
 
-router.delete("/api/planReview/removeReviewer", (req, res) => {
-  const plan_id = req.body.plan_id;
-  const reviewer_id = req.body.reviewer_id;
-  plans
-    .findById(plan_id)
-    .then((plan) => {
-      const reviewer_index = plan.reviewers.findIndex(
-        (r) => r.user_id === reviewer_id
-      );
-      if (reviewer_index >= 0) {
-        //remove reviwer_id from the plan
-        plan.reviewers.splice(reviewer_index, 1);
-        //remove plan_id from reviewer's doc
-        users
-          .findById(reviewer_id)
-          .then((user) => {
-            const plan_index = user.whitelisted_plan_ids.indexOf(plan_id);
-            user.whitelisted_plan_ids.splice(plan_index, 1);
-            user.save();
-          })
-          .catch((err) => errorHandler(err));
-        plan.save();
-      } else {
-        errorHandler(res, 404, {
-          message: "Reviewer does not exist for this plan.",
-        });
-      }
-      returnData(plan, res);
-    })
-    .catch((err) => errorHandler(err));
+/*
+  Return a list of reviews of the reviewer with populated reviewee info
+*/
+router.get("/api/planReview/plansToReview", (req, res) => {
+  const reviewer_id = req.query.reviewer_id;
+  planReviews
+    .find({ reviewer_id })
+    .populate("reviewee_id", "name email affiliation school grade")
+    .then((reviews) => returnData(reviews, res))
+    .catch((err) => errorHandler(res, 500, err));
+});
+
+router.delete("/api/planReview/removeReview", (req, res) => {
+  const review_id = req.query.review_id;
+  planReviews
+    .findByIdAndDelete(review_id)
+    .then((review) => returnData(review, res))
+    .catch((err) => errorHandler(res, 500, err));
 });
 
 module.exports = router;
