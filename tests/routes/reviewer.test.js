5f682b05926c5dcfef071760a2be5551a290ca61
Matthew Liu <mliu78@jh.edu>
2022-03-19 12:07:25 -0400

    fix: removed comment

--- tests/routes/reviewer.test.js
+++ tests/routes/reviewer.test.js
@@ -14,19 +14,15 @@ beforeEach(async () => {
 describe(`Test endpoint ${endpoint}`, () => {
   describe("HTTP POST request", () => {
     test("Return 401 if missing plan_id or reviewer_id in request body", async () => {
-        const response1 = await request.post(`${endpoint}`).send({
-            plan_id: "asdfjksla"
-        });
-        const response2 = await request.post(`${endpoint}`).send({
-            reviewer_id: "asdfjkl"
-        });
-        expect(response1.status).toBe(401);
-        expect(response2.status).toBe(401);
+      const response1 = await request.post(`${endpoint}`).send({
+        plan_id: "asdfjksla",
+      });
+      const response2 = await request.post(`${endpoint}`).send({
+        reviewer_id: "asdfjkl",
+      });
+      expect(response1.status).toBe(401);
+      expect(response2.status).toBe(401);
     });
-
-    // test("Return 402 if reviewer already added on plan", async () => {
-
-    // });
 
     test("Return 200 if successfully added reviewer", async () => {
       const response = await request.post(`${endpoint}`).send({
@@ -35,7 +31,6 @@ describe(`Test endpoint ${endpoint}`, () => {
       });
       expect(response.status).toBe(200);
     });
-
   });
 });
 
