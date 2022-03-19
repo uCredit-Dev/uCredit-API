a9775681c5773892d918db7efcb87e05de4a8cce
Matthew Liu <mliu78@jh.edu>
2022-03-19 14:45:59 -0400

    merge conflicts resolved

--- model/User.js
+++ model/User.js
@@ -8,14 +8,7 @@ const userSchema = new Schema({
   affiliation: { type: String }, //STUDENT, FACULTY or STAFF
   school: { type: String },
   grade: { type: String }, //AE UG Sophomore
-<<<<<<< HEAD
   plan_ids: [{ type: Schema.Types.ObjectId, ref: "Plan", default: [] }],
-=======
-  plan_ids: [{ type: Schema.Types.ObjectId, ref: "Plan" }],
-  whitelisted_plan_ids: [
-    { type: Schema.Types.ObjectId, ref: "Plan", default: [] },
-  ],
->>>>>>> parent of b5c25f9 (Merge pull request #27 from uCredit-Dev/iter-02-dockerize)
 });
 
 const User = mongoose.model("User", userSchema);
