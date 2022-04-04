8563936f7bfd1490f06e50a41d0048a6bb2cfe61
Qinyuan Wu <qinyuan.wupsn@gmail.com>
2022-04-04 17:17:08 -0400

    changed user_id field of Notification to an array

--- model/Notification.js
+++ model/Notification.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
     This model refers to a user's notification.
 */
 const notificationSchema = new Schema({
-  user_id: { type: String, required: true },
+  user_id: { type: [String], required: true },
   message: { type: String, required: true },
   quick_link_id: { type: Schema.Types.ObjectId },
   link_type: { type: String, enum: ["PLAN", "REVIEW", "USER", "DISTRIBUTION"] },
--- routes/notification.js
+++ routes/notification.js
@@ -10,7 +10,7 @@ router.get("/api/notifications/:user_id", (req, res) => {
   if (!user_id) {
     errorHandler(res, 400, { message: "Must provide user_id." });
   }
-  Notifications.find({ user_id })
+  Notifications.find({ user_id: { $elemMatch: user_id } })
     .then((notifications) => {
       returnData(notifications, res);
     })
