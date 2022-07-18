c02960cb6b26dc13167d1db7325eba67e30415b1
mpark63 <minspk13@gmail.com>
2022-07-18 17:24:34 +0900

    update dependencies for jest to work with ts

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
+const { defaults: tsjPreset } = require('ts-jest/presets')
+
 module.exports = {
   preset: "@shelf/jest-mongodb",
   verbose: true,
   collectCoverage: true,
   coverageDirectory: "coverage",
   testEnvironment: "node",
+  transform: tsjPreset.transform,
 };
