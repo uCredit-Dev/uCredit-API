a614971671951b06eaf48403ed1a39ebc904f301
AkhilDeo <singingbird99@gmail.com>
2022-07-25 15:57:37 -0400

    Inject Majors into Production code done

--- routes/major.js
+++ routes/major.js
@@ -1,5 +1,7 @@
 const majors = require("../model/Major.js");
 const { returnData, errorHandler } = require("./helperMethods.js");
+const { allMajors } = require("../data/majors");
+
 
 const express = require("express");
 const router = express.Router();
@@ -19,6 +21,15 @@ router.post("/api/majors", async (req, res) => {
       returnData(major, res);
     })
     .catch((err) => errorHandler(res, 400, err));
+});
+
+// Once we add all majors to the majors.js file, we can use this code to add all majors to production DB
+router.post("/api/majors/all", async (req, res) => {
+  for (let m of allMajors) {
+    await majors
+    .create(m)
+    .catch((err) => errorHandler(res, 400, err));
+  }
 });
 
 module.exports = router;
