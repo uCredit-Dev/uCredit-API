8f7133f2d1c23d03956124ea83cce80db72bd566
Matthew Liu <mliu78@jh.edu>
2022-08-01 00:53:05 -0700

    feat: nodemailer implementation

--- routes/planReview.js
+++ routes/planReview.js
@@ -1,5 +1,10 @@
-const { returnData, errorHandler, postNotification } = require("./helperMethods.js");
+const {
+  returnData,
+  errorHandler,
+  postNotification,
+} = require("./helperMethods.js");
 const planReviews = require("../model/PlanReview.js");
+const nodemailer = require("nodemailer");
 
 const express = require("express");
 const router = express.Router();
@@ -12,7 +17,8 @@ router.post("/api/planReview/request", async (req, res) => {
   const reviewer_id = req.body.reviewer_id;
   if (!plan_id || !reviewer_id || !reviewee_id) {
     errorHandler(res, 400, {
-      message: "Missing plan_id, reviewee_id or reviewer_id in the request body.",
+      message:
+        "Missing plan_id, reviewee_id or reviewer_id in the request body.",
     });
   } else {
     const review = await planReviews
@@ -124,7 +130,13 @@ router.get("/api/planReview/plansToReview", (req, res) => {
 router.post("/api/planReview/changeStatus", (req, res) => {
   const review_id = req.body.review_id;
   const status = req.body.status;
-  if (!(status === "REJECTED" || status === "APPROVED" || status === "UNDERREVIEW")) {
+  if (
+    !(
+      status === "REJECTED" ||
+      status === "APPROVED" ||
+      status === "UNDERREVIEW"
+    )
+  ) {
     errorHandler(res, 400, {
       message: "Invalid status. Must be APPROVED, REJECTED, or UNDERREVIEW.",
     });
@@ -140,6 +152,7 @@ router.post("/api/planReview/changeStatus", (req, res) => {
         review.status = status;
         if (status === "UNDERREVIEW") review.requestTime = Date.now();
         review.save();
+        sendReviewMail(review.reviewee_id, review.reviewer_id, review.plan_id);
         postNotification(
           `A plan review status has changed to ${status}.`,
           [review.reviewee_id],
@@ -151,6 +164,39 @@ router.post("/api/planReview/changeStatus", (req, res) => {
     })
     .catch((err) => errorHandler(res, 500, err));
 });
+
+// async..await is not allowed in global scope, must use a wrapper
+async function sendReviewMail(reviewee_id, reviewer_id, plan_id) {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  // let testAccount = await nodemailer.createTestAccount();
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      type: "OAuth2",
+      user: "ucreditdev@gmail.com",
+      clientId:
+        "258377223314-218vsftjcq46a2l43e81hesoo4unmdjg.apps.googleusercontent.com",
+      clientSecret: "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",
+      refreshToken:
+        "1//04imE9pIG-khOCgYIARAAGAQSNwF-L9IrTuI4rKvAo2l8oPA0_HIZhUB98vmO0pvH__GOqn7n3bvNK-s-DcQcaFBr-77OdcsOtj4",
+      accessToken:
+        "ya29.A0AVA9y1sHpW6M88rODXcxlIA7CLXZzT6wyk8Co9HLo8-Da7--qGXnnXJaIDBpB6uUM1BIKa9Jt0VXtorob7HwBTQjHaINxz0uHON0pDgVFdc9CwKNc46DCNJFo5DvthPzvAPnJlCIh2a0G_zL8mHIX_cLZbJ5YUNnWUtBVEFTQVRBU0ZRRTY1ZHI4TDFnMmlYVVhpZGpYNW9SdW5EWFUtdw0163",
+      expires: 1484314697598,
+    },
+  });
+
+  // send mail with defined transport object
+  await transporter.sendMail({
+    from: "uCredit", // sender address
+    to: "mattliujhu@gmail.com", // list of receivers
+    subject: `Invitation to review from ${reviewee_id}`, // Subject line
+    html: `<div><p>Hello ${reviewer_id},</p><p>You have recieved a request to review ${reviewee_id}'s uCredit Plan:</p><p>Please click the following link to accept.</p><p>https://ucredit.me/reviewer/${plan_id}</p><p>Best wishes,</p><p>uCredit</p</div>`, // html body
+  });
+}
 
 router.delete("/api/planReview/removeReview", (req, res) => {
   const review_id = req.query.review_id;
