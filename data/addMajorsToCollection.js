de5db0f0c567fb1fb041da1feef5fccc864d49cf
mpark63 <minspk13@gmail.com>
2022-10-06 17:05:31 -0400

    wrote updateAllMajors()

--- data/addMajorsToCollection.js
+++ data/addMajorsToCollection.js
@@ -10,16 +10,54 @@ const users = require("../model/User.js");
 const { allMajors } = require("./majors");
 const majors = require("../model/Major");
 
-addAllMajors();
+// addAllMajors(); 
+updateAllMajors(); 
 
 // Add ALL majors in allMajors array to production DB
 async function addAllMajors() {
   await db.connect(); // comment out this line before running jest test
-  majors.insertMany(allMajors).catch((err) => console.log(err));
-  console.log(allMajors.length.toString() + " majors added.");
-  console.log(
-    "Done! Check DB to confirm the documents have been added to the collection."
-  );
+  let new_count = 0; 
+  for (let major of allMajors) {
+    const existing_major = await majors.findById(major); 
+    if (!existing_major) {
+      majors.insertMany(major)
+        .then((res) => {
+          console.log(`added new degree: ${res.data}`); 
+          new_count++; 
+        })
+        .catch((err) => console.log(err)); 
+    } else {
+      console.log(`${major._id} already exists!`); 
+    }
+  }
+  console.log(console.log(`Done adding ${new_count} new majors! Check DB to confirm the documents have been added to the collection`));
+}
+
+// Add ALL majors in allMajors array to production DB
+async function updateAllMajors() {
+  await db.connect(); // comment out this line before running jest test
+  let new_count = 0; 
+  let update_count = 0; 
+  for (let major of allMajors) {
+    const existing_major = await majors.findById(major._id); 
+    if (!existing_major) {
+      majors.insertMany(major)
+        .then((res) => {
+          console.log(`added new degree: ${major._id}`); 
+          new_count++; 
+        })
+        .catch((err) => console.log(err)); 
+    } else {
+      existing_major.remove(); 
+      majors.insertMany(major)
+        .then((res) => {
+          console.log(`updated existing degree: ${major._id}`); 
+          update_count++; 
+        })
+        .catch((err) => console.log(err)); 
+    }
+  }
+  console.log(console.log(`Done adding ${new_count} new majors and updating ${update_count} existing majors! Check DB to confirm.`));
 }
 
 // Add a specified major in allMajors array to production DB
