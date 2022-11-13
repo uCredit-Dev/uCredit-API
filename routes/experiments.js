7dfed44e5895609071128395bf632407b7f653e6
mpark63 <minspk13@gmail.com>
2022-11-13 01:23:26 -0500

    DEV-43: added comments to health check path

--- routes/experiments.js
+++ routes/experiments.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const ExperimentDao = require("../data/ExperimentDao");
 const ApiError = require("../model/ApiError");
+const majors = require("../model/Major.js");
 const { returnData } = require("./helperMethods.js");
 
 const router = express.Router();
 const experiments = new ExperimentDao();
 
+// health check path that allows render to confirm server is up 
+// restart app automatically if unresponsive or errors
 router.get("/api/test", (req, res) => {
-  returnData("test passed", res);
+  // simple fail proof db query 
+  majors
+    .findOne({})
+    .then((major) => returnData(major, res))
+    .catch((err) => errorHandler(res, 500, err));
 });
 
 router.get("/api/experiments/allExperiments", async (req, res, next) => {
