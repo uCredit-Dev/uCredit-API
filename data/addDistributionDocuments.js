c11acae447e8e6fa9cf112b4d72005f0e836e729
AkhilDeo <singingbird99@gmail.com>
2022-11-12 20:55:16 -0500

    Didn't actually finish addDistributionsDocuments

--- data/addDistributionDocuments.js
+++ data/addDistributionDocuments.js
@@ -6,12 +6,4 @@ const distributions = require("../model/Distribution.js");
 // Delete all Distributions Documents
 async function addDistributionsDocuments() {
   await db.connect(); // comment out this line before running jest test
-  distributions
-    .deleteMany({})
-    .then(() => {
-      console.log("Distributions documents deleted");
-    })
-    .catch((error) => {
-      console.log(error);
-    });
 }
