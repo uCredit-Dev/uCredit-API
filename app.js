3074c5a3e492cb90f8dc2abf476043fcff2718fb
Miseok Kim <amcdrmkr@gmail.com>
2023-02-19 12:48:53 -0500

    remove bugsnag - Minseo

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ import evalRouter from "./routes/evaluation.js";
 import sisRouter from "./routes/sisData.js";
 import experimentsRouter from "./routes/experiments.js";
 import commentRouter from "./routes/comment.js";
-import Bugsnag from "@bugsnag/js";
-import BugsnagPluginExpress from "@bugsnag/plugin-express";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -39,17 +37,6 @@ export default function createApp() {
     credentials: true,
   };
 
-  // error report
-  Bugsnag.start({
-    apiKey: process.env.BUGSNAG_API_KEY,
-    plugins: [BugsnagPluginExpress],
-  });
-
-  Bugsnag.notify("lanching app");
-  const middleware = Bugsnag.getPlugin("express");
-
-  //use middleware functions
-  app.use(middleware.requestHandler); //must keep as the first piece of middleware in the stack
   app.use(cors(corsOptions));
   app.use(helmet());
   app.use(morgan("dev"));
@@ -68,8 +55,5 @@ export default function createApp() {
   app.use(sisRouter);
   app.use(experimentsRouter);
   app.use(commentRouter);
-  // This handles any errors that Express catches. This needs to go before other
-  // error handlers. Bugsnag will call the `next` error handler if it exists.
-  app.use(middleware.errorHandler);
   return app;
 }
