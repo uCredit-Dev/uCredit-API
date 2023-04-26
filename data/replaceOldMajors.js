a2382e01329b0874d35024444a1351fd4585b8bd
mpark63 <minspk13@gmail.com>
2023-04-25 20:48:34 -0400

    run-prettier: run prettier

--- data/replaceOldMajors.js
+++ data/replaceOldMajors.js
@@ -1,4 +1,4 @@
-import { connect } from "./db.js";
+import { connect } from './db.js';
 import User from '../model/User.js';
 import Plan from '../model/Plan.js';
 
@@ -13,54 +13,81 @@ async function replaceOldWithNew() {
         // console.log("before: " + plan.majors);
         for (let ind in plan.majors) {
           let major = plan.majors[ind];
-          if (major.includes("B.A. Computer Science (NEW - 2021 & after)")) {
-            plan.majors[ind] = "B.A. Computer Science";
-          } else if (major.includes("B.S. Computer Science (OLD - Pre-2021)") 
-          || major.includes("B.S. Computer Science (NEW - 2021 & after)")) {
-            plan.majors[ind] = "B.S. Computer Science";
-          } else if (major.includes("Minor Computer Science (OLD - Pre-2021)")) {
-            plan.majors[ind] = "Minor Computer Science";
-          } else if (major.includes("Minor Computer Science (NEW - 2021 & after)")) {
-            plan.majors[ind] = "Minor Computer Science";
-          } else if (major.includes("Minor Applied Mathematics & Statistics (OLD - Pre-2021)")
-          || major.includes("Minor Applied Mathematics & Statistics (NEW - 2021 & after)")) {
-            plan.majors[ind] = "Minor Applied Mathematics & Statistics";
-          } 
+          if (major.includes('B.A. Computer Science (NEW - 2021 & after)')) {
+            plan.majors[ind] = 'B.A. Computer Science';
+          } else if (
+            major.includes('B.S. Computer Science (OLD - Pre-2021)') ||
+            major.includes('B.S. Computer Science (NEW - 2021 & after)')
+          ) {
+            plan.majors[ind] = 'B.S. Computer Science';
+          } else if (
+            major.includes('Minor Computer Science (OLD - Pre-2021)')
+          ) {
+            plan.majors[ind] = 'Minor Computer Science';
+          } else if (
+            major.includes('Minor Computer Science (NEW - 2021 & after)')
+          ) {
+            plan.majors[ind] = 'Minor Computer Science';
+          } else if (
+            major.includes(
+              'Minor Applied Mathematics & Statistics (OLD - Pre-2021)',
+            ) ||
+            major.includes(
+              'Minor Applied Mathematics & Statistics (NEW - 2021 & after)',
+            )
+          ) {
+            plan.majors[ind] = 'Minor Applied Mathematics & Statistics';
+          }
         }
-        plan.majors =  [...new Set(plan.majors)];
+        plan.majors = [...new Set(plan.majors)];
         // console.log("after: " + plan.majors + "");
         // console.log("done!\n");
       }
-      if (plan !== null && plan.major_ids != null && plan.major_ids.length > 0) {
-        console.log("before: " + plan.major_ids);
+      if (
+        plan !== null &&
+        plan.major_ids != null &&
+        plan.major_ids.length > 0
+      ) {
+        console.log('before: ' + plan.major_ids);
         for (let ind in plan.major_ids) {
           let major = plan.major_ids[ind];
-          if (major.includes("B.A. Computer Science (NEW - 2021 & after)")) {
-            plan.major_ids[ind] = "B.A. Computer Science";
-          } else if (major.includes("B.S. Computer Science (OLD - Pre-2021)") 
-          || major.includes("B.S. Computer Science (NEW - 2021 & after)")) {
-            plan.major_ids[ind] = "B.S. Computer Science";
-          } else if (major.includes("Minor Computer Science (OLD - Pre-2021)")) {
-            plan.major_ids[ind] = "Minor Computer Science";
-          } else if (major.includes("Minor Computer Science (NEW - 2021 & after)")) {
-            plan.major_ids[ind] = "Minor Computer Science";
-          } else if (major.includes("Minor Applied Mathematics & Statistics (OLD - Pre-2021)")
-          || major.includes("Minor Applied Mathematics & Statistics (NEW - 2021 & after)")) {
-            plan.major_ids[ind] = "Minor Applied Mathematics & Statistics";
-          } 
+          if (major.includes('B.A. Computer Science (NEW - 2021 & after)')) {
+            plan.major_ids[ind] = 'B.A. Computer Science';
+          } else if (
+            major.includes('B.S. Computer Science (OLD - Pre-2021)') ||
+            major.includes('B.S. Computer Science (NEW - 2021 & after)')
+          ) {
+            plan.major_ids[ind] = 'B.S. Computer Science';
+          } else if (
+            major.includes('Minor Computer Science (OLD - Pre-2021)')
+          ) {
+            plan.major_ids[ind] = 'Minor Computer Science';
+          } else if (
+            major.includes('Minor Computer Science (NEW - 2021 & after)')
+          ) {
+            plan.major_ids[ind] = 'Minor Computer Science';
+          } else if (
+            major.includes(
+              'Minor Applied Mathematics & Statistics (OLD - Pre-2021)',
+            ) ||
+            major.includes(
+              'Minor Applied Mathematics & Statistics (NEW - 2021 & after)',
+            )
+          ) {
+            plan.major_ids[ind] = 'Minor Applied Mathematics & Statistics';
+          }
         }
-        plan.major_ids =  [...new Set(plan.major_ids)];
-        console.log("after: " + plan.major_ids + "");
-        console.log("done!\n");
+        plan.major_ids = [...new Set(plan.major_ids)];
+        console.log('after: ' + plan.major_ids + '');
+        console.log('done!\n');
       }
       await plan.save();
     }
   }
-  console.log("done")
+  console.log('done');
 }
 
 replaceOldWithNew();
-
 
 async function checkResult() {
   connect();
--- data/updatePrereq.js
+++ data/updatePrereq.js
@@ -1,53 +1,59 @@
-import { connect } from "./db.js";
-import dotenv from "dotenv";
+import { connect } from './db.js';
+import dotenv from 'dotenv';
 import SISCourseV from '../model/SISCourseV.js';
 
 dotenv.config();
 
 async function updateIntroAlgo() {
   connect();
-  const number = "EN.601.433";
+  const number = 'EN.601.433';
   const oldPrereq = [
-      {
-          "Description": "EN.600.226/EN.601.226 AND (EN.553.171/EN.550.171 OR EN.553.172/EN.550.170 OR EN.600.271/EN.601.231",
-          "Expression": "EN.600.226[C]^AND^(^EN.553.171[C]^OR^EN.553.172[C]^OR^EN.600.271[C]^)",
-          "IsNegative": "N"
-      },
-      {
-          "Description": "Students may receive credit for only one of EN.600.363, EN.600.463, EN.601.433, EN.601.633.",
-          "Expression": "EN.600.363[C]^OR^EN.601.633[C]",
-          "IsNegative": "Y"
-      }
+    {
+      Description:
+        'EN.600.226/EN.601.226 AND (EN.553.171/EN.550.171 OR EN.553.172/EN.550.170 OR EN.600.271/EN.601.231',
+      Expression:
+        'EN.600.226[C]^AND^(^EN.553.171[C]^OR^EN.553.172[C]^OR^EN.600.271[C]^)',
+      IsNegative: 'N',
+    },
+    {
+      Description:
+        'Students may receive credit for only one of EN.600.363, EN.600.463, EN.601.433, EN.601.633.',
+      Expression: 'EN.600.363[C]^OR^EN.601.633[C]',
+      IsNegative: 'Y',
+    },
   ];
 
   const newPrereq = [
-      {
-          "Description": "EN.600.226/EN.601.226 AND (EN.553.171/EN.550.171 OR EN.553.172/EN.550.170 OR EN.600.271/EN.601.231 OR EN.601.230",
-          "Expression": "EN.600.226[C]^AND^(^EN.553.171[C]^OR^EN.553.172[C]^OR^EN.600.271[C]^OR^EN.601.230[C]^)",
-          "IsNegative": "N"
-      },
-      {
-          "Description": "Students may receive credit for only one of EN.600.363, EN.600.463, EN.601.433, EN.601.633.",
-          "Expression": "EN.600.363[C]^OR^EN.601.633[C]",
-          "IsNegative": "Y"
-      }
+    {
+      Description:
+        'EN.600.226/EN.601.226 AND (EN.553.171/EN.550.171 OR EN.553.172/EN.550.170 OR EN.600.271/EN.601.231 OR EN.601.230',
+      Expression:
+        'EN.600.226[C]^AND^(^EN.553.171[C]^OR^EN.553.172[C]^OR^EN.600.271[C]^OR^EN.601.230[C]^)',
+      IsNegative: 'N',
+    },
+    {
+      Description:
+        'Students may receive credit for only one of EN.600.363, EN.600.463, EN.601.433, EN.601.633.',
+      Expression: 'EN.600.363[C]^OR^EN.601.633[C]',
+      IsNegative: 'Y',
+    },
   ];
 
-  let data = await SISCourseV.find({number});
+  let data = await SISCourseV.find({ number });
   let course = data[0];
   for (let version of course.versions) {
     version.preReq = newPrereq;
   }
   await course.save();
-  console.log("done!")
+  console.log('done!');
 }
 
 // updateIntroAlgo();
 
 async function checkUpdated() {
   connect();
-  const number = "EN.601.433";
-  let data = await SISCourseV.find({number});
+  const number = 'EN.601.433';
+  let data = await SISCourseV.find({ number });
   let course = data[0];
 
   for (let version of course.versions) {
@@ -56,8 +62,6 @@ async function checkUpdated() {
 }
 
 checkUpdated();
-
-
 
 /** Intro Algo
  * {
