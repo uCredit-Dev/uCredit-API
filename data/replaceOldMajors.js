49e888c548681e35995e59c4603de94483e7bfed
mpark63 <minspk13@gmail.com>
2023-05-06 01:51:55 +0900

    DIS: update replaceOldMajors script

--- data/replaceOldMajors.js
+++ data/replaceOldMajors.js
@@ -10,40 +10,6 @@ async function renameOldWithNew() {
   for (let user of users) {
     for (let plan_id of user.plan_ids) {
       let plan = await Plan.findById(plan_id).exec();
-      if (plan !== null && plan.majors != null && plan.majors.length > 0) {
-        // console.log("before: " + plan.majors);
-        for (let ind in plan.majors) {
-          let major = plan.majors[ind];
-          if (major.includes('B.A. Computer Science (NEW - 2021 & after)')) {
-            plan.majors[ind] = 'B.A. Computer Science';
-          } else if (
-            major.includes('B.S. Computer Science (OLD - Pre-2021)') ||
-            major.includes('B.S. Computer Science (NEW - 2021 & after)')
-          ) {
-            plan.majors[ind] = 'B.S. Computer Science';
-          } else if (
-            major.includes('Minor Computer Science (OLD - Pre-2021)')
-          ) {
-            plan.majors[ind] = 'Minor Computer Science';
-          } else if (
-            major.includes('Minor Computer Science (NEW - 2021 & after)')
-          ) {
-            plan.majors[ind] = 'Minor Computer Science';
-          } else if (
-            major.includes(
-              'Minor Applied Mathematics & Statistics (OLD - Pre-2021)',
-            ) ||
-            major.includes(
-              'Minor Applied Mathematics & Statistics (NEW - 2021 & after)',
-            )
-          ) {
-            plan.majors[ind] = 'Minor Applied Mathematics & Statistics';
-          }
-        }
-        plan.majors = [...new Set(plan.majors)];
-        // console.log("after: " + plan.majors + "");
-        // console.log("done!\n");
-      }
       if (
         plan !== null &&
         plan.major_ids != null &&
@@ -80,15 +46,15 @@ async function renameOldWithNew() {
         }
         plan.major_ids = [...new Set(plan.major_ids)];
         console.log('after: ' + plan.major_ids + '');
-        console.log('done!\n');
+        console.log(`updated plan: ${plan.name}\n`);
+        await plan.save();
       }
-      await plan.save();
     }
   }
   console.log('done');
 }
 
-replaceOldWithNew();
+renameOldWithNew();
 
 async function checkResult() {
   connect();
